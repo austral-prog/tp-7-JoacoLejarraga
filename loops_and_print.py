f9af11a29357a78d805b7c0f8df32d2733da9780
JoacoLejarraga <162721835+JoacoLejarraga@users.noreply.github.com>
2024-05-17 11:09:50 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,27 @@
-def enumerate_list(list):
-    return list
+colors = ["Red", "Green", "", "White", "Black"]
+
+def enumerate_list(lista):
+    index = 0
+    
+    while index < len(lista):
+        if lista[index]:
+            word = lista[index]
+            lista[index] = f"{index}. {word}"
+            index += 1
+        else:
+            del lista[index]
+    return lista
 
 
-def enumerate_backwards(list):
-    return list
+
+def enumerate_backwards(lista):
+    index = 0
+    
+    while index < len(lista):
+        if lista[index]:
+            word = lista[index][-1::-1]
+            lista[index] = f"{index}. {word}"           
+            index += 1
+        else:
+            del lista[index]
+    return lista
